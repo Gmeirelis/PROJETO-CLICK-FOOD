96f5a6d53bb879d39845fe618a631826a9a2f21c
guilherme <gmeirelis8@gmail.com>
2025-11-20 16:48:19 -0400

    quarto  commit

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
+
+let index = 0;
 const slides = document.querySelectorAll(".slide");
-let currentIndex = 0;
 
-function nextSlide() {
-  slides[currentIndex].removeAttribute("data-active");
-  currentIndex = (currentIndex + 1) % slides.length;
-  slides[currentIndex].setAttribute("data-active", "");
+function trocarSlide() {
+  index++;
+  if (index >= slides.length) index = 0;
+
+  document.querySelector(".slides").style.transform =
+    `translateX(${-index * 100}%)`;
 }
 
-setInterval(nextSlide, 3000);
+setInterval(trocarSlide, 4000);
 
-
-/* filtro busca*/
-
-const campoFiltro = document.getElementById('filtro');
-  const restaurantes = document.querySelectorAll('.restaurante');
-
-  campoFiltro.addEventListener('keyup', () => {
-    const texto = campoFiltro.value.toLowerCase();
-
-    restaurantes.forEach(restaurante => {
-      const nome = restaurante.querySelector('h3').textContent.toLowerCase();
-
-     if (nome.includes(texto)) {
-  restaurante.style.removeProperty("display");
-} else {
-  restaurante.style.display = "none";
-}
-    });
-  });
 
   
 
